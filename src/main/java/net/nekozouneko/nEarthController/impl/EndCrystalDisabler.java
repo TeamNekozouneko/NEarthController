7f2a964c8c3b593616554d0ace43ff1fe233d5e7
るんく君 <91005690+RunqRun@users.noreply.github.com>
2026-03-15 03:45:16 +0900

    fix: ダメージが無効化されないバグを修正

--- src/main/java/net/nekozouneko/nEarthController/impl/EndCrystalDisabler.java
+++ src/main/java/net/nekozouneko/nEarthController/impl/EndCrystalDisabler.java
@@ -1,8 +1,10 @@
 package net.nekozouneko.nEarthController.impl;
 
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityExplodeEvent;
 
 public class EndCrystalDisabler implements Listener {
@@ -11,5 +13,15 @@ public class EndCrystalDisabler implements Listener {
         if(e.getEntityType() == EntityType.END_CRYSTAL){
             e.setCancelled(true);
         }
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent e){
+        Entity directEntity = e.getDamageSource().getDirectEntity();
+
+        if(directEntity == null) return;
+        if(directEntity.getType() != EntityType.END_CRYSTAL) return;
+
+        e.setCancelled(true);
     }
 }
